2ebfa623ffb8ca2c0bc6b2651ba1aea7c97b5bf2
Karel Asselman <asselman.karel@gmail.com>
2021-03-22 21:04:28 +0100

    ProjectController: index, create + form

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProjectRepository;
 
 class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'project')]
-    public function index(): Response
+    #[Route('/project', name: 'app_project', methods: ['GET'])]
+    public function index(ProjectRepository $projectRepository): Response
     {
+        $projects = $projectRepository->findBy(['owner' => $this->getUser()->getId()]);
+
         return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+            'projects' => $projects,
         ]);
+    }
+
+    #[Route('/project/create', name: 'app_project_create', methods: ['POST'])]
+    public function create(): Response
+    {
+        dd($request->request);
     }
 }
