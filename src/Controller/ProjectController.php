6df28928b34f76540efe8ecdedef4c141f67f875
Karel Asselman <asselman.karel@gmail.com>
2021-03-23 08:45:10 +0100

    create project/new template

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+
 use App\Repository\ProjectRepository;
 
 class ProjectController extends AbstractController
@@ -19,9 +23,28 @@ class ProjectController extends AbstractController
         ]);
     }
 
-    #[Route('/project/create', name: 'app_project_create', methods: ['POST'])]
-    public function create(): Response
+    #[Route('/project/new', name: 'app_project_new', methods: ['GET'])]
+    public function new()
     {
-        dd($request->request);
+        return $this->render('project/new.html.twig');
+    }
+
+    #[Route('/project/create', name: 'app_project_create', methods: ['POST'])]
+    public function create(ProjectRepository $projectRepository, CsrfTokenManagerInterface $csrfTokenManager, Request $request): Response
+    {
+        $token = new CsrfToken('create-project', $request->get("_csrf_token"));
+        if (!$csrfTokenManager->isTokenValid($token)) {
+            throw new InvalidCsrfTokenException();
+        }
+        // TODO: validations
+
+        $errors = [];
+        $errors = array("No validations yet");
+        $projects = $projectRepository->findBy(['owner' => $this->getUser()->getId()]);
+
+        return $this->render('project/index.html.twig', [
+            'projects' => $projects,
+            'errors' => $errors
+        ]);
     }
 }
